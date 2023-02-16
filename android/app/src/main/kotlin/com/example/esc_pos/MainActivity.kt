c5ac8bbc37e0f6f225fd1c0bea7cbde67ec88b83
SEANGLAY <seanglyseanglay@gmail.com>
2023-02-16 10:42:06 +0700

    initiate

---
+++ android/app/src/main/kotlin/com/example/esc_pos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.esc_pos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/esc_pos.dart
@@ -0,0 +1,10 @@
+library esc_pos;
+
+
+export './src/barcode.dart';
+export './src/capability_profile.dart';
+export './src/enums.dart';
+export './src/pos_column.dart';
+export './src/pos_styles.dart';
+export './src/qrcode.dart';
+export './src/generator.dart';
